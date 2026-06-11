78d0af4fca2bcf0112cc070e64350f8e8095bee9
Damsi119 <nayanama.d2000@gmail.com>
2026-06-11 20:25:34 +0530

    Scaffold admin results page

---
+++ admin/view_results.php
@@ -0,0 +1,12 @@
+﻿<?php
+include "../includes/auth_check.php";
+checkRole('admin');
+include "../includes/header.php";
+?>
+<div class="card">
+    <h2>All Results</h2>
+    <div class="toolbar">
+        <a href="dashboard.php" class="btn btn-outline">Back</a>
+    </div>
+</div>
+<?php include "../includes/footer.php"; ?>
