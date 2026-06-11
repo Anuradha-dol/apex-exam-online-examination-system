7f4d4c895c98f1d040245428984727a0f3735644
Damsi119 <nayanama.d2000@gmail.com>
2026-06-11 20:25:35 +0530

    Scaffold student results page

---
+++ student/my_results.php
@@ -0,0 +1,12 @@
+﻿<?php
+include "../includes/auth_check.php";
+checkRole('student');
+include "../includes/header.php";
+?>
+<div class="card">
+    <h2>My Results</h2>
+    <div class="toolbar">
+        <a href="dashboard.php" class="btn btn-outline">Back</a>
+    </div>
+</div>
+<?php include "../includes/footer.php"; ?>
