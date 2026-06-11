78b8052923588ff343554766df0cbbfe7b70dad3
Damsi119 <nayanama.d2000@gmail.com>
2026-06-11 20:25:35 +0530

    Scaffold lecturer results page

---
+++ lecturer/view_results.php
@@ -0,0 +1,12 @@
+﻿<?php
+include "../includes/auth_check.php";
+checkRole('lecturer');
+include "../includes/header.php";
+?>
+<div class="card">
+    <h2>Student Results</h2>
+    <div class="toolbar">
+        <a href="dashboard.php" class="btn btn-outline">Back</a>
+    </div>
+</div>
+<?php include "../includes/footer.php"; ?>
